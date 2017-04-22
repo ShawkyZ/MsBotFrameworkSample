58c6ec3a9b9506aad146585e3bc7d160066509b3
Abdelrhman Shawky <abdelrhmanshawky4@gmail.com>
2017-04-22 16:51:06 +0200

    Add more properties for the Feedback model

--- TestBot/Models/Feedback.cs
+++ TestBot/Models/Feedback.cs
@@ -3,14 +3,24 @@ using Microsoft.Bot.Builder.FormFlow;
 
 namespace TestBot.Models
 {
+    public enum State
+    {
+        Happy = 1,
+        Sad
+    }
     [Serializable]
+    [Template(TemplateUsage.NotUnderstood, "Sorry, I didn't get {0}.", "I didn't understand {0}")]
+    [Template(TemplateUsage.NoPreference, "No Preference", "No Preference")]
+    [Template(TemplateUsage.Integer, "Choose a number from 1 to 5", "Choose a number from 1 to 5")]
     public class Feedback
     {
         [Numeric(1, 5)]
-        [Describe("Rate your experience with us from 1-5")]
-        public int StarsNumber { get; set; }
+        [Prompt("Rate your experience with us from 1-5")]
+        public int Rating { get; set; }
         [Optional]
-        [Describe("Wanna send us a message?")]
+        [Prompt("Choose a {&}. {||}")]
+        public State? State { get; set; }
+        [Prompt("Tell us why.")]
         public string AdditionalMessage { get; set; }
         public static IForm<Feedback> BuildForm()
         {
